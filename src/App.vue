3d3e8b2b40a9cd4f4872e5bb1fe1e5db0d47a175
joypyo <mybrownie_@naver.com>
2022-04-03 22:55:22 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="(a) in 메뉴들" :key="a">{{a}}</a>
+
+  </div>
+
+<div v-for="test in 3" :key="test">
+  <h4 >{{products[0]}}</h4>
+  <p >{{price[0]}}</p>
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
+  data(){
+    return {
+      메뉴들 : ['Home', 'Shop', 'About'],
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      price : ['70','50','30'],
+    }
+     
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -21,6 +35,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding : 15px;
+  border-radius: 5px;
+}
+.menu a{
+  color: white;
+  padding: 10px;
 }
 </style>
